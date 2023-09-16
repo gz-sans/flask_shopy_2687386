436173cec3bf0617d088836ae089df8328d01cd7
gz-sans <juancamilotorresavila268@gmail.com>
2023-09-15 22:26:02 -0500

    que tal

--- app/config.py
+++ app/config.py
@@ -1,4 +1,4 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:@localhost/flask_shopy_2687386"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:1234@localhost:3311/flask_shopy_2687386"
     SQLALCHEMY_TRACK_MODIFICATIONS =False 
     SECRET_KEY = 'ficha'
